f87860da1fa8d8a038906653de2d2263f69e3e51
Sooraj Gupta <soorajgupta00@gmail.com>
2025-05-29 13:31:32 +0530

    date formatting & sorting

---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+export function formatData(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return new Date(date).toLocaleDateString(undefined, options);
+}
